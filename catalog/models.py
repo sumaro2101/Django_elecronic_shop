7de5bbb55be1bef024b13b4802b7bf6f3c298800
Alex Pavlov <yukiu217@gmail.com>
2024-04-21 00:04:29 +0600

    add empty models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    def __str__(self) -> str:
+        return f'{self.category}: {self.name}'
+    
+    class Meta:
+        verbose_name = 'товаров'
+        varbose_name_plural = 'товары'
+        
+class Category(models.Model):
+    def __str__(self) -> str:
+        return self.category
+    
+    class Meta:
+        verbose_name = 'катерогий'
+        varbose_name_plural = 'категории'
+        
