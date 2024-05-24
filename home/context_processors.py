be8cb20bd7138a447c2d48bcd3ab6e67703fd6a5
Alex Pavlov <yukiu217@gmail.com>
2024-05-24 22:57:00 +0600

    fix errors empty site

--- home/context_processors.py
+++ home/context_processors.py
@@ -2,7 +2,10 @@ from home.models import BasePageModel
 from catalog.models import Product
 
 def get_base_model(request):
-    return {'base_model': BasePageModel.objects.select_related('nav_bar', 'nav_left', 'nav_right', 'footer').get(pk=1)}
+    
+    base_model = BasePageModel
+    base_model = base_model.objects.select_related('nav_bar', 'nav_left', 'nav_right', 'footer').get(pk=1) if base_model.objects.all().exists() else None
+    return {'base_model': base_model}
 
 def get_product(request):
     return {'product': Product.objects.select_related('company', 'category')}
--- home/templatetags/nav_tags.py
+++ home/templatetags/nav_tags.py
@@ -8,10 +8,9 @@ register = template.Library()
 
 @register.inclusion_tag('base/nav_bar.html', takes_context=True)
 def show_nav_bar(context ,base_model , cat_selected=1):
-
-    text = base_model
-
-    return {
+    try:
+        text = base_model
+        base = {
         'nav_items': text.nav_list.get_queryset(),
         'title': text.nav_bar.title,
         'mask_search': text.nav_bar.mask_search,
@@ -19,14 +18,27 @@ def show_nav_bar(context ,base_model , cat_selected=1):
         'cat_selected': cat_selected,
         'user': context.request.user
         }
+    except:
+        base = {
+        'nav_items': 'В разработке' ,
+        'title': 'В разработке',
+        'mask_search': 'Продукт',
+        'button': 'Поиск',
+        'cat_selected': 1,
+        'user': context.request.user,
+        }
+
+    return base
     
 @register.inclusion_tag('base/left_bar.html')
 def show_left_bar(base_model):
-    
-    title_nav = base_model.nav_left.title
+    try:
+        title_nav = base_model.nav_left.title
+    except:
+        title_nav = 'Категории'
 
     return {
-        'title_nav': title_nav.title,
+        'title_nav': title_nav,
         }
     
 @register.inclusion_tag('base/list_catalog.html')
@@ -46,28 +58,43 @@ def show_left_list_bar():
     
 @register.inclusion_tag('base/right_bar.html')
 def show_right_bar(base_model, product):
+    try:
+        title_nav = base_model.nav_right.title
+        end_lines = base_model.nav_right.end_lines
+    except:
+        title_nav = 'Новинки'
+        end_lines = 'В разработке'
     
-    title_nav = base_model.nav_right.title
     list_new_items = product.values('name', 'release', 'url')
-    end_lines = base_model.nav_right.end_lines
     
-    return {
+    right_bar = {
         'title_nav': title_nav,
         'time': timezone.now() - timedelta(180),
         'new_items': list_new_items,
         'end_lines': end_lines,
         }
+    
+    return right_bar
 
 
 @register.inclusion_tag('base/footer.html')
 def show_footer(base_model):
     
-    footer_items = base_model.footer
-    
-    return {
+    try:
+        footer_items = base_model.footer
+        footer = {
         'company_name': footer_items.name_company,
         'block': footer_items.footerblocks_set.all(),
         'policy': footer_items.policy,
         'url_policy': footer_items.url_policy,
         }
+    except:
+        footer = {
+        'company_name': 'EL_COM',
+        'block': 'В разработке',
+        'policy': 'В разработке',
+        'url_policy': 'В разработке',
+        }
+    
+    return footer
     
